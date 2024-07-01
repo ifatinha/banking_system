126c6241f8c1d065df243dd00eab5ba9cbe697af
ifatinha <ifatinha99@gmail.com>
2024-07-01 07:52:36 -0300

    correção do gerador de logs

--- util/Decoratores.py
+++ util/Decoratores.py
@@ -6,8 +6,8 @@ def log_operations(type_operation):
     
         def wrapper(*args, **kwargs):
         
-            print(f"=== Tipo de transação: {type_operation} ===")
             operation(*args, **kwargs)
+            print(f"=== Tipo de transação: {type_operation} ===")
             print(f"=== Realizado em {datetime.now().strftime("%d/%m/%Y %H:%M")} ===")
 
         return wrapper
